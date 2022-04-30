75e0edc27e936cc9e832ac97a3c826711c8c3094
Denuka Dissanayaka <www.denukadissanayaka@gmail.com>
2022-04-30 19:39:12 +0530

    initialise project

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import myPic from '../../asset/denuka.jpg'
+import "./sidebar.css";
+
+export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get("/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src={myPic}
+          alt=""
+        />
+        <p>
+        I am a web developer with an amazing ability to develop websites that are both functional and aesthetically pleasing.
+         I have a strong understanding of web standards and best practices, and I am passionate about creating websites that users will find easy to use and visually appealing.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          {cats.map((c) => (
+            <Link to={`/?cat=${c.name}`} className="link">
+            <li className="sidebarListItem">{c.name}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
